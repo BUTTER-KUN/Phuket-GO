0ef575d87e80245e11e2f751a2aa045be58dd0ae
BUTTER-KUN <138361074+Phuket-KUN@users.noreply.github.com>
2023-08-20 14:05:38 +0700

    mainscene

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-Rigidbody2D body;
+   public float moveSpeed = 5f;
 
-float horizontal;
-float vertical;
+   public Rigidbody2D rb;
+   public Animator animator;
 
-public float runSpeed = 20.0f;
+   Vector2 movement;
 
-void Start ()
-{
-   body = GetComponent<Rigidbody2D>(); 
+   void Update()
+   {
+      movement.x = Input.GetAxisRaw("Horizontal");
+      movement.y = Input.GetAxisRaw("Vertical");
+
+      animator.SetFloat("horizontal", movement.x);
+      animator.SetFloat("vertical", movement.y);
+      animator.SetFloat("Speed", movement.sqrMagnitude);
+   }
+
+   void FixedUpdate()
+   {
+      rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+   }
 }
 
-void Update ()
-{
-   horizontal = Input.GetAxisRaw("Horizontal");
-   vertical = Input.GetAxisRaw("Vertical"); 
-}
-
-private void FixedUpdate()
-{  
-   body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
-}
-}
