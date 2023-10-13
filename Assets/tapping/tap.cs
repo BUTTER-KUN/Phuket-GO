f11a326394d21bb201b6386a21c01df1297d6822
Adhipat <adhipat555@gmail.com>
2023-10-13 15:52:58 +0700

    funtion click ไม่ขึ้น

---
+++ Assets/tapping/tap.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tap : MonoBehaviour
+{
+    public Text hitCount;
+    private int hit = 100;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    public void count()
+    {
+        hit--;
+        hitCount.text = "Health: " + hit;
+    }
+}
+
