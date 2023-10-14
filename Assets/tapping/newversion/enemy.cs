ef97b66c66cefe789de0dc18b61077f643092e36
Adhipat <adhipat555@gmail.com>
2023-10-14 12:00:15 +0700

    gameplay scene

--- Assets/tapping/newversion/enemy.cs
+++ Assets/tapping/newversion/enemy.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image healthBar;
+    public float health = 10f;
+    public Animator swordAnimator; // Reference to the Animator component of the sword animation object
+
+    private void Start()
     {
-        
+        // Assuming the sword animation object is a child of the enemy
+        swordAnimator = GetComponentInChildren<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnMouseDown()
     {
+        TakeDamage();
         
+        // Trigger the attack animation
+        swordAnimator.SetTrigger("attack");
+        
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+    public void TakeDamage()
+    {
+        health--;
+        healthBar.fillAmount = health / 10f;
     }
 }
